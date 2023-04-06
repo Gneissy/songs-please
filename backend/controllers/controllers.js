a67fa592e0a8b885808308a8d9e0d72428c06682
Gneissy <ahmetserteserr@gmail.com>
2023-04-06 21:03:20 +0300

    fix v3

--- backend/controllers/controllers.js
+++ backend/controllers/controllers.js
@@ -139,8 +139,10 @@ const postInputSongs = async function (req, res){
                       console.error(err);
                     });
             }
-            res.status(200).redirect(`${backendServer}/recommendations?userId=${userId}&inputSongs=${inputSong1},${inputSong2},${inputSong3}`);
+            res.status(200);
         }
+
+        // redirect(`${backendServer}/recommendations?userId=${userId}&inputSongs=${inputSong1},${inputSong2},${inputSong3}`);
 
         // If none of the 3 input songs are valid
         else {
