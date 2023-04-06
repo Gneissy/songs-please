497b2a1f24772789b5f027e5c081302d0dd3d4b2
Gneissy <ahmetserteserr@gmail.com>
2023-04-06 21:55:09 +0300

    fix v6 | works for localhost

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,8 +10,8 @@ import { v4 as uuidv4 } from 'uuid';
 
 function App(){
 
-  const backendServer = "https://songs-please.onrender.com";
-  // const backendServer = "http://localhost:3000";
+  // const backendServer = "https://songs-please.onrender.com";
+  const backendServer = "http://localhost:3000";
 
   // Personal usage for every single user
   const random = localStorage.getItem('random') || uuidv4();
